91a474598bf0abe2b2efc0c1a289100a73493796
Dmitrij <dmitrijsemenkin@gmail.com>
2023-07-06 23:48:54 +0300

    update storage

--- internal/repository/storage/storage.go
+++ internal/repository/storage/storage.go
@@ -1,18 +1,18 @@
 package storage
 
 import (
-	"github.com/Masterminds/squirrel"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/golang-test-task/pkg/databases/postgresql"
 )
 
 type Storage struct {
 	client       postgresql.Client
-	queryBuilder squirrel.StatementBuilderType
+	queryBuilder sq.StatementBuilderType
 }
 
-func NewStorage(client postgresql.Client, queryBuilder squirrel.StatementBuilderType) *Storage {
+func NewStorage(client postgresql.Client) *Storage {
 	return &Storage{
 		client:       client,
-		queryBuilder: queryBuilder,
+		queryBuilder: sq.StatementBuilder.PlaceholderFormat(sq.Dollar),
 	}
 }
