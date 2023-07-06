f4a12cec76c7ff2a7ea066a19b12f03ade00a21f
Dmitrij <dmitrijsemenkin@gmail.com>
2023-07-07 01:02:14 +0300

    update error repository

--- internal/repository/error.go
+++ internal/repository/error.go
@@ -4,4 +4,7 @@ import "github.com/golang-test-task/pkg/common/errors"
 
 var ErrNotFound = errors.New("not found")
 var ErrNothingInserted = errors.New("nothing inserted")
-var ErrIntegrityConstraintViolation = errors.New("unique violation")
+var ErrNothingDeleted = errors.New("nothing deleted")
+var ErrNothingUpdated = errors.New("nothing updated")
+
+//var ErrIntegrityConstraintViolation = errors.New("unique violation")
