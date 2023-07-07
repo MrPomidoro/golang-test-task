3def733b137e9f8a289a9fca29f56ba6d8641ba3
Dmitrij <dmitrijsemenkin@gmail.com>
2023-07-07 11:59:42 +0300

    create send email service

---
+++ pkg/service/email/email.go
@@ -0,0 +1,41 @@
+package email
+
+import "github.com/go-gomail/gomail"
+
+type EmailService interface {
+	SendHTML(message, subject, receiver string) error
+}
+
+type emailService struct {
+	dialer   *gomail.Dialer
+	sender   string
+	host     string
+	port     int
+	username string
+	password string
+}
+
+func NewEmailService(sender, host string, port int, username, password string) *emailService {
+	return &emailService{
+		sender:   sender,
+		host:     host,
+		port:     port,
+		username: username,
+		password: password,
+		dialer:   gomail.NewDialer(host, port, username, password),
+	}
+}
+
+func (s *emailService) SendHTML(message, subject, receiver string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", s.sender)
+	m.SetHeader("To", receiver)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", message)
+
+	err := s.dialer.DialAndSend(m)
+	if err != nil {
+		return err
+	}
+	return nil
+}
